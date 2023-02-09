d8a6b6facc08d9c014bdfa476431b6221a936fdf
Torres-sam <samuelvianatorres@gmial.com>
2023-02-09 15:16:03 -0300

    Renomeado arquivos e add novos exercicios

---
+++ 3.Exercícios(entrada, processamento, saída)/Exercicio1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor inteiro: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um valor inteiro: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            int soma = n1 + n2;
+
+            Console.WriteLine($"A soma entre os valores {n1} e {n2} é: {soma}");
+        }
+    }
+}
