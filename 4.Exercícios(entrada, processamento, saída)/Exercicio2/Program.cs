f9bf26b003746b264ccb8cdfdce722a22c4d3cd6
Torres-sam <samuelvianatorres@gmial.com>
2023-02-09 23:24:58 -0300

    Add Exercicios de sáida de Dados

---
+++ 4.Exercícios(entrada, processamento, saída)/Exercicio2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicio2 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor para o raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double area = 3.14159 * Math.Pow(raio, 2.0);
+            Console.WriteLine("O valor da area é :" + area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
