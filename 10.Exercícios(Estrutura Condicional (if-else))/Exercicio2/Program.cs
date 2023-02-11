930a816c4ea2d79603abd644599b9824c2c66ec8
Torres-sam <samuelvianatorres@gmial.com>
2023-02-11 00:24:57 -0300

    Add exercicios if-else

---
+++ 10.Exercícios(Estrutura Condicional (if-else))/Exercicio2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicio2 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("Par!");
+            }
+            else {
+                Console.WriteLine("Ímpar");
+            }
+        }
+    }
+}
---
+++ 13.Exercícios(Estrutura Condicional (if-else))/Exercicio5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------MENU------------");
+            Console.WriteLine("[1] Cachorro Quente R$ 4.00\n\r" +
+                "[2] X-Salada........R$ 4.50\n\r" +
+                "[3] X-Bacon.........R$ 5.00\n\r" +
+                "[4] Torrada Simples R$ 2.00\n\r" +
+                "[5] Refrigerante....R$ 1.50\n\r" +
+                "----------------------------");
+            Console.WriteLine("Escolha uma opçao do Cardapio: ");
+            int codigo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quantidade: ");
+            int quant = int.Parse(Console.ReadLine());
+            double pedido = 0.0;
+            if (codigo == 1)
+            {
+                pedido = 4.0 * (double)quant;
+            }
+            else if (codigo == 2)
+            {
+                pedido = 4.5 * quant;
+            }
+            else if (codigo == 3)
+            {
+                pedido = 5.0 * quant;
+            }
+            else if (codigo == 4)
+            {
+                pedido = 2.0 * quant;
+            }
+            else if (codigo == 5)
+            {
+                pedido = 1.5 * quant;
+            }
+            else {
+                Console.WriteLine("Código Invalido!!");
+            }
+            Console.WriteLine("Total a pagar: R$ " + pedido.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
