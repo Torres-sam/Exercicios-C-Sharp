12a8e7e50ab3aee7580024ed70f58ed55e235301
Torres-sam <samuelvianatorres@gmial.com>
2023-02-12 20:28:05 -0300

    Add Exercicios while

---
+++ 17.Exercícios(Estrutura Repetitiva WHILE)/Eercicio1/Eercicio1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a senha: ");
+            int senha = int.Parse(Console.ReadLine());
+            int tentativa = 1;
+            while (senha != 2002 && tentativa < 3)
+            {
+                tentativa++;
+                Console.Write("Senha Invalida ");
+                Console.WriteLine("Tentativa (" + tentativa + ")");
+                Console.WriteLine("Digite a senha novamente: ");
+                senha = int.Parse(Console.ReadLine());
+
+            }
+
+            if (senha == 2002)
+            {
+                Console.WriteLine("Acesso Permitido");
+            }
+            else
+            {
+                Console.WriteLine("Senha Bloqueada, Tente novamente mais tarde");
+            }
+        }
+    }
+}
