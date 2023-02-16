7667992beb9c428042733811b4fd44be0bf29d44
Torres-sam <samuelvianatorres@gmial.com>
2023-02-15 22:44:52 -0300

    Adicionado Exercicios

---
+++ 12.Exercícios(Estrutura Condicional (if-else))/Exercicio4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hora inicial: ");
+            int hora_inicial = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hora final: ");
+            int hora_final = int.Parse(Console.ReadLine());
+
+            int duracao;
+
+            if (hora_inicial < hora_final)
+            {
+                duracao = hora_inicial - hora_final;
+            }
+            else 
+            {
+                duracao = 24 - hora_inicial + hora_final;
+            }
+            Console.WriteLine($"A duração do jogo foi de {duracao} hrs");
+        }
+    }
+}
---
+++ 6.Exercícios(entrada, processamento, saída)/Exercicio4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Número do funcionario: ");
+            int num_funci = int.Parse(Console.ReadLine());
+            Console.WriteLine("Horas trabalhadas: ");
+            int num_horas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valor por horas: ");
+            double valor_trab = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salario = num_horas * valor_trab;
+
+            Console.WriteLine("Número do funcionario: " + num_funci);
+            Console.WriteLine("Salario do funcionario: " + salario.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
